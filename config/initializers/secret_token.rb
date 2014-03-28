272eb436e298477f13ddbd578f4447bce82c4c5f
davidakimbo <minheeman@gmail.com>
2014-03-28 12:20:04 -0400

    VMP first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VegMePlease::Application.config.secret_key_base = '518a3f45ef6dbfed6a4f98379cd2516173295f73c9e75671b12aa976cf8c75a27d1a19ef1832dbe9f0506e1fc4eda27b461140f5b74ec43e340192fbbc7e70ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VegMePlease::Application.config.session_store :cookie_store, key: '_veg_me_please_session'
