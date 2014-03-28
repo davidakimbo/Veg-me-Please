4c031290d17a592f1b3720718644a955a9a119bc
davidakimbo <minheeman@gmail.com>
2014-03-28 15:46:32 -0400

    second commit

---
+++ app/models/recipe.rb
@@ -0,0 +1,3 @@
+class recipe < ActiveRecord::Base
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 VegMePlease::Application.routes.draw do
 
-get '/vegme' => 'vegme#index'
+get '/recipes' => 'recipes#index'
 
-root to: 'vegme#index'
+root to: 'recipes#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
