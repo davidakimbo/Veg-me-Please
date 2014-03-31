a90ea5b2069806180c19d6dfd51c7d097ae1d5a6
davidakimbo <minheeman@gmail.com>
2014-03-31 19:45:22 -0400

    Added sweet/salty feature and cooking time feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,15 @@
 class RecipesController < ApplicationController
 
   def index
-    @a = Recipe.testing_queries
+    @test_a = Recipe.testing_queries
   end
 
-  def show
-    #Pass the acquired params back to the recipe model for calculating!
-
-    @b = Recipe.recipe_search(
-      params[:choice_simple],
-      params[:sweet_savory],
-      params[:preffered_veggie],
-      params[:allergies])
-  binding.pry
+  def show  #Pass the acquired params back to the recipe model for calculating!
+      choice_simple = params[:choice_simple]
+      sweet_savory = params[:sweet_savory]
+      preferred_veggie = params[:preferred_veggie]
+      allergies = params[:allergies]
+      @results = Recipe.recipe_search(choice_simple, sweet_savory, preferred_veggie, allergies)
   end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,11 +7,30 @@ class Recipe < ActiveRecord::Base
   end
 
 
-  def self.recipe_search(choice_simple, sweet_savory, preffered_veggie, allergies)
+  def self.recipe_search(choice_simple, sweet_savory, preferred_veggie, allergies)
 
-    @recipe = HTTParty.get("http://api.yummly.com/v1/api/recipes?_app_id=9a96c9a6&_app_key=4e268421d281dd0f9ab5c56532f44642&q=#{preffered_veggie}") #Insert user params here
+    if choice_simple == "true"
+      choice_simple = "&maxTotalTimeInSeconds=900"
+    else
+      choice_simple = "&maxTotalTimeInSeconds=5400"
+    end
+
+    if sweet_savory == "sweet"
+      sweet_savory = "&flavor.sweet.min=0.4&flavor.sweet.max=1&"
+    elsif sweet_savory == "meaty"
+      sweet_savory = "&flavor.meaty.min=0.4&flavor.meaty.max=1&"
+    end
+
+
+    if allergies
+      allergies.each do |allergy|
+
+    else
+      end
+
     binding.pry
-    return @recipe["matches"][0]
+
+    @recipe = HTTParty.get("http://api.yummly.com/v1/api/recipes?_app_id=9a96c9a6&_app_key=4e268421d281dd0f9ab5c56532f44642&requirePictures=true&allowedDiet[]=387&q=#{preferred_veggie}#{choice_simple}#{sweet_savory}") #Insert user params here
   end
 
   def self.recipe_skim() #This will skim the response and trim it with more parameters
